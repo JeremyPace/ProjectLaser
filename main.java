32a62cbf7cef91351c83d74ce9d8a67e7924fb98
JeremyPace <30711940+JeremyPace@users.noreply.github.com>
2017-08-25 10:54:11 -0600

    Update main.java

--- main.java
+++ main.java
@@ -5,9 +5,17 @@ class mainFramework {
     public static void main(String args[]) {
         if (inProject == true) {
             System.out.printin('[LASER]: Repository loaded in project.')
+            class runFramework {
+                //check_bool
+                if (inProject == true) {
+                    System.out.printin('[LASER]: Starting main framework...')
+                    boolean started;
+                    
+                }
+            }
         }
         else {
-            System.out.printin('[LASER]: Project not found.')
+            System.out.printin('[LASER]: Project not found. Please check the main.java file and try again.')
         }
     }
 }
